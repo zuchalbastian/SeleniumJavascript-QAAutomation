0cd96bc0d4805d1b21e7de2765bf7b8131630a7f
zuchalbastian <assasinbastian022@gmail.com>
2025-02-05 10:54:58 +0700

    Open website churchmarshal.com

---
+++ Tests/Dummy.java
@@ -0,0 +1,29 @@
+package Tests;
+
+public class Dummy {
+    public static void main(String[] args) {
+        String danger = "Bastian";
+        System.out.println("String value: " + danger);
+
+        int stuff = 9;
+        System.out.println("Int value: " + stuff);
+
+        long stuff1 = 1200000;
+        System.out.println("Long value: " + stuff1);
+
+        float stuff2 = 35.2f;
+        System.out.println("Float value: " + stuff2);
+
+        double stuff3 = 35.78;
+        System.out.println("Double value: " + stuff3);
+        
+        boolean answer = true;
+        System.out.println("Boolean value: " + answer);
+
+        boolean noanswer = false;
+        System.out.println("Boolean value: " + noanswer);
+
+        char letter = 'a';
+        System.out.println("Char value: " + letter);
+    }
+}
--- Tests/QACourse1.js
+++ Tests/QACourse1.js
@@ -1,9 +1,11 @@
 const { Builder, By, Key } = require("selenium-webdriver");
 const driver = new Builder().forBrowser("chrome").build();
-
+driver.manage().window().maximize();
 
 
 async function example() {
+    await driver.get("https://cmnewtr.churchmarshal.com/index.php");
+    await driver.quit();
 
 }
 
